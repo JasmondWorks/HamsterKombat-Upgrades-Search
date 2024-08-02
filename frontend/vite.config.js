a3a13dff54b983bc3a9bf1dbf9dbab75c3d1663e
Obafemi Olorede <obafemilared@gmail.com>
2024-08-02 08:15:03 +0100

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base:
-      mode === "production"
-        ? "https://hamsterkombat-upgrades-search-5.onrender.com"
-        : "/",
-    define: {
-      __DEV__: mode === "development",
-      __PROD__: mode === "production",
-    },
-    build: {
-      outDir: "dist",
-      sourcemap: mode === "development",
-    },
-    server: {
-      port: 3200,
-    },
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
 });
